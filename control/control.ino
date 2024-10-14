bfd53ed470f24d3cc5ffffa0b8d59c55198ec9ff
codernr <zoltan.csore@protonmail.com>
2024-10-14 08:21:51 +0200

    Simplify code to one big if

--- control/control.ino
+++ control/control.ino
@@ -10,48 +10,14 @@
 #define UPDATES_PER_SECOND 10
 
 CRGB leds[NUM_LEDS];
-CRGB color;
 unsigned int t_min;
 uint8_t minutes;
 uint8_t hours;
-
-Word* words[] = {
-  new MinuteWord(1, 132, 3, leds), // "egy" has length 3
-  new MinuteWord(2, 138, 5, leds), // "ketto" has length 5
-  new MinuteWord(3, 127, 5, leds), // "harom" has length 5
-  new MinuteWord(4, 120, 4, leds), // "negy" has length 4
-  new MinuteWord(5, 108, 2, leds), // "ot" has length 2
-  new MinuteWord(6, 112, 3, leds), // "hat" has length 3
-  new MinuteWord(7, 117, 3, leds), // "het" has length 3
-  new MinuteTextWord(104, 4, leds), // "perc" has length 4
-  new PastWord(101, 3, leds), // "cel" has length 3
-  new PastWord(96, 4, leds), // "mult" has length 4
-  new ToWord(86, 5, leds), // "mulva" has length 5
-  new HalfWord(93, 3, leds), // "fel" has length 3
-  new QuarterPrefixWord(79, 5, leds), // "harom" has length 5
-  new QuarterWord(73, 6, leds), // "negyed" has length 6
-  new HourWord(1, 60, 3, leds), // "egy" has length 3
-  new HourWord(2, 64, 5, leds), // "ketto" has length 5
-  new HourWord(3, 55, 5, leds), // "harom" has length 5
-  new HourWord(4, 51, 4, leds), // "negy" has length 4
-  new HourWord(5, 22, 2, leds), // "ot" has length 2
-  new HourWord(6, 69, 3, leds), // "hat" has length 3
-  new HourWord(7, 48, 3, leds), // "het" has length 3
-  new HourWord(8, 36, 5, leds), // "nyolc" has length 5
-  new HourWord(9, 42, 6, leds), // "kilenc" has length 6
-  new HourWord(10, 33, 3, leds), // "tiz" has length 3
-  new HourWord(11, 24, 8, leds), // "tizenegy" has length 8
-  new HourWord(12, 12, 10, leds), // "tizenketto" has length 10
-  new NoonWord(9, 3, leds), // "del" has length 3
-  new MidnightWord(4, 5, leds), // "ejfel" has length 5
-  new HourTextWord(0, 3, leds), // "ora" has length 3
-};
 
 void setup() {
   Serial.begin(9600);
   delay( 3000 ); // power-up safety delay
   t_min = 0;
-  color = CRGB::Blue;
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
 }
@@ -64,7 +30,6 @@ void loop() {
   hours = (t_min / 60);
   minutes = (t_min - hours * 60);
 
-  FastLED.clear();
   update(hours, minutes);
   FastLED.show();
   t_min = t_min + 1;
@@ -72,9 +37,7 @@ void loop() {
 }
 
 void update(uint8_t h, uint8_t m) {
-  //Serial.print("T[" + String(h) + ":" + String(m) + "]");
-  for (Word* word : words) {
-    word->update(h, m, color);
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    leds[i] = getPixel(i, h, m);
   }
-  //Serial.println();
 }
--- control/words.h
+++ control/words.h
@@ -4,171 +4,57 @@
 #include <Arduino.h>
 #include <FastLED.h>
 
-class Word
+extern CRGB color;
+
+bool minuteActive(const uint8_t minute, const uint8_t m)
 {
-public:
-    Word(uint8_t start, uint8_t length, CRGB* leds)
-        : content(content), length(length), start(start), leds(leds) {}
-    
-    virtual bool isActive(uint8_t h, uint8_t m) const = 0;
-    
-    virtual ~Word() {}
+    return m % 15 == minute || m % 15 == 15 - minute;
+}
 
-    void update(uint8_t h, uint8_t m, CRGB color)
-    {
-        if (isActive(h, m))
-        {
-            Serial.print(" " + content);
-            for (uint8_t i = 0; i < length; ++i)
-            {
-                leds[start + i] = color;
-            }
-        }
-    }
-
-protected:
-    String content;
-    uint8_t length;
-    uint8_t start;
-    CRGB *leds;
-};;
-
-class MinuteWord : public Word
+bool hourActive(const uint8_t hour, const uint8_t h, const uint8_t m)
 {
-public:
-    MinuteWord(uint8_t minute, uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds), minute_(minute) {}
+    return (h % 12 == hour - 1 && m > 7) || (h % 12 == hour && m < 8);
+}
 
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return m % 15 == minute_ || m % 15 == 15 - minute_;
-    }
-
-private:
-    uint8_t minute_;
-};
-
-class HourWord : public Word
+CRGB getPixel(const uint8_t i, const uint8_t h, const uint8_t m)
 {
-public:
-    HourWord(uint8_t hour, uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds), hour_(hour) {}
+    if (
+        (i > 131 && i < 135 && minuteActive(1, m)) ||
+        (i > 137 && i < 143 && minuteActive(2, m)) ||
+        (i > 126 && i < 132 && minuteActive(3, m)) ||
+        (i > 119 && i < 124 && minuteActive(4, m)) ||
+        (i > 107 && i < 110 && minuteActive(5, m)) ||
+        (i > 111 && i < 115 && minuteActive(6, m)) ||
+        (i > 116 && i < 120 && minuteActive(7, m)) ||
 
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return (h % 12 == hour_ - 1 && m > 7) || (h % 12 == hour_ && m < 8);
-    }
+        (i > 103 && i < 108 && m % 15 != 0) ||
+        (i > 100 && i < 104 && m % 15 > 0 && m % 15 < 8) ||
+        (i > 95 && i < 100 && m % 15 > 0 && m % 15 < 8) ||
+        (i > 85 && i < 91 && m % 15 > 7 && m % 15 != 0) ||
+        (i > 92 && i < 96 && m > 22 && m < 38) ||
+        (i > 78 && i < 84 && m > 37 && m < 53) ||
+        (i > 72 && i < 79 && m % 30 > 7 && m % 30 < 23) ||
 
-private:
-    uint8_t hour_;
-};
+        (i > 59 && i < 63 && hourActive(1, h, m)) ||
+        (i > 63 && i < 69 && hourActive(2, h, m)) ||
+        (i > 54 && i < 60 && hourActive(3, h, m)) ||
+        (i > 50 && i < 55 && hourActive(4, h, m)) ||
+        (i > 21 && i < 24 && hourActive(5, h, m)) ||
+        (i > 68 && i < 72 && hourActive(6, h, m)) ||
+        (i > 47 && i < 51 && hourActive(7, h, m)) ||
+        (i > 35 && i < 41 && hourActive(8, h, m)) ||
+        (i > 41 && i < 48 && hourActive(9, h, m)) ||
+        (i > 32 && i < 36 && hourActive(10, h, m)) ||
+        (i > 23 && i < 32 && hourActive(11, h, m)) ||
+        (i > 11 && i < 24 && hourActive(12, h, m)) ||
 
-class NoonWord : public Word
-{
-public:
-    NoonWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
+        (i > 8 && i < 12 && ((h == 11 && m > 52) || (h == 12 && m < 8))) ||
+        (i > 3 &&i < 9 && ((h == 23 && m > 52) || (h == 0 && m < 8))) ||
+        (i >=0 && i < 4 && ((h != 11 && h != 23 && m > 53) || (h != 0 && h != 12 && m < 8)))
+    )
+    return CRGB::Blue;
 
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return (h == 11 && m > 52) || (h == 12 && m < 8);
-    }
-};
-
-class MidnightWord : public Word
-{
-public:
-    MidnightWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
-
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return (h == 23 && m > 52) || (h == 0 && m < 8);
-    }
-};
-
-class HourTextWord : public Word
-{
-public:
-    HourTextWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
-
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return (h != 11 && h != 23 && m > 53) || (h != 0 && h != 12 && m < 8);
-    }
-};
-
-class MinuteTextWord : public Word
-{
-public:
-    MinuteTextWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
-
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return m % 15 != 0;
-    }
-};
-
-class QuarterPrefixWord : public Word
-{
-public:
-    QuarterPrefixWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
-
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return m > 37 && m < 53;
-    }
-};
-
-class QuarterWord : public Word
-{
-public:
-    QuarterWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
-
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return m % 30 > 7 && m % 30 < 23;
-    }
-};
-
-class HalfWord : public Word
-{
-public:
-    HalfWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
-
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return m > 22 && m < 38;
-    }
-};
-
-class PastWord : public Word
-{
-public:
-    PastWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
-
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return (m % 15 > 0 && m % 15 < 8);
-    }
-};
-
-class ToWord : public Word
-{
-public:
-    ToWord(uint8_t start, uint8_t length, CRGB* leds)
-        : Word(start, length, leds) {}
-
-    bool isActive(uint8_t h, uint8_t m) const override
-    {
-        return m % 15 > 7 && m % 15 != 0;
-    }
-};
+    return CRGB::Black;
+}
 
 #endif // RULES_H
